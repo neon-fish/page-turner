24130c39997a91682d81ae8efc23db496547b1f5
Ed Johnson <ed@neon.fish>
2022-02-06 09:07:50 +0000

    Move dev game settings into dev game folder

--- src/dev-game/index.ts
+++ src/dev-game/index.ts
@@ -1,36 +1,12 @@
 import { Mcge } from '../engine';
 import '../engine/styles.css';
-import { IMAGE_URL } from './2/assets';
 import { pages } from './2/pages';
-// import { state } from './2/state';
-import { theme } from './2/theme';
+import { settings } from './2/settings';
 
 // Entry point into the app from index.html
 
 const game = new Mcge({
-  settings: {
-    containerEl: "#mcge",
-    debug: true,
-    content: {
-      top: "5%",
-      height: "45%",
-      // blur: true,
-      // tint: true,
-      // autoNext: true,
-      delay: 20,
-    },
-    choices: {
-      tint: true,
-      left: "calc((100% - min(80%, 600px)) / 2)",
-      width: "min(80%, 600px)",
-      top: "calc(100% - min(35%, 250px))",
-      height: "min(35%, 250px)",
-    },
-    images: {
-      // defaultBgImage: IMAGE_URL.cyberpunkStreet,
-    },
-    theme: theme,
-  },
+  settings: settings,
   pages: pages,
   state: {}, // state,
 });
